2446160bfa1a32f0a0775d4f64daec96881fb0d1
Sarthak Kundra <sarthakkundra21@gmail.com>
2021-03-13 16:42:16 +0530

    sign in error

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+  } from 'react-native-google-signin';
 
-import "firebase/app";
+  import {signIn, signOut} from './auth';
 
-// import app from './firebaseConfig.js';
-// console.log(app)
+
+
+const [loggedIn, setloggedIn] = useState(false);
+const [userInfo, setuserInfo] = useState([]);
+
+useEffect(() => {
+  GoogleSignin.configure({
+    scopes: ['email'], // what API you want to access on behalf of the user, default is email and profile
+    webClientId:
+      '418977770929-g9ou7r9eva1u78a3anassxxxxxxx.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  });
+}, []);
 const handleLogin = () => {
 
 console.log("TEst")
@@ -14,8 +30,22 @@ console.log("TEst")
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title="Login with Google" onPress={handleLogin} />
+      <Text>Open up App.tsx to start working on your app!!!</Text>
+      <GoogleSigninButton
+                style={{width: 192, height: 48}}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={signIn}
+              />
+              <Text> Testing 123</Text>
+
+{!loggedIn && <Text>You are currently logged out</Text>}
+              {loggedIn && (
+                <Button
+                  onPress={signOut}
+                  title="LogOut"
+                  color="red"></Button>
+              )}
       <StatusBar style="auto" />
     </View>
   );
--- app/auth.js
+++ app/auth.js
@@ -1,29 +1,31 @@
-var provider = new firebase.auth.GoogleAuthProvider();
+   export const signIn = async() => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const {accessToken, idToken} = await GoogleSignin.signIn();
+      setloggedIn(true);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        alert('Cancel');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        alert('Signin in progress');
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        alert('PLAY_SERVICES_NOT_AVAILABLE');
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  }
 
-export const login = () => {
-  firebase.auth()
-.signInWithPopup(provider)
-.then((result) => {
-  /** @type {firebase.auth.OAuthCredential} */
-  var credential = result.credential;
-
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = credential.accessToken ;
-  // The signed-in user info.
-  var user = result.user;
-
-  console.log(`User ${user}`);
-  // ...
-}).catch((error) => {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-
-  console.error(errorMessage);
-});
-}
+  export const signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      setloggedIn(false);
+      setuserInfo([]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
